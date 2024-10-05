cba60dadc0aae7cbdf075fbd312a30972ae77bf5
Sjaiod <delwar.saj2008@gmail.com>
2024-10-05 15:26:55 +0600

    d

--- components/admin/MemberSERCH.tsx
+++ components/admin/MemberSERCH.tsx
@@ -138,7 +138,7 @@ const MemberSearch = () => {
             <p className="text-red-500">{error}</p>
           ) : (
             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-6">
-              {members && members.map((member: any) => (
+              {members && members.map((member: Member) => (
                 <MemberCard key={member.id} member={member} />
               ))}
             </div>
--- components/admin/event/FirstPageEVENTS.tsx
+++ components/admin/event/FirstPageEVENTS.tsx
@@ -113,7 +113,7 @@ const EventSearch = () => {
             <p className="text-red-500">{error}</p>
           ) : (
             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-6">
-              {events && events.map((event) => (
+              {events && events.map((event:Event) => (
                 <EventCard key={event.id} event={event} />
               ))}
             </div>
